6b83075be712cb94cc5ca07cfa335a22082cee80
EDU <0narara0@gmail.com>
2018-10-25 12:49:03 +0900

    commit

--- app/src/main/java/com/cmy/www/birthplacebutton/MainActivity.java
+++ app/src/main/java/com/cmy/www/birthplacebutton/MainActivity.java
@@ -7,40 +7,48 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button button7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button7 = (Button)findViewById(R.id.button7);
+
     }
-    public void onclickwidget(View view){
-        int id = view.getId();
-        String text;
+
+    public void onclickwidget1(View view){
+//        int id = view.getId();
+        String text = ((Button)view).getText().toString();
         TextView textView = (TextView)findViewById(R.id.textview2);
 
-        switch(id){
-            case  R.id.button1:
-                text = ((Button)view).getText().toString();
-                break;
-            case  R.id.button2:
-                text = ((Button)view).getText().toString();
-                break;
-            case  R.id.button3:
-                text = ((Button)view).getText().toString();
-                break;
-            case  R.id.button4:
-                text = ((Button)view).getText().toString();
-                break;
-            case  R.id.button5:
-                text = ((Button)view).getText().toString();
-                break;
-            case  R.id.button6:
-                text = ((Button)view).getText().toString();
-                break;
-                default:
-                    text = "Nothing";
+//        switch(id){
+//            case  R.id.button1:
+//                text = ((Button)view).getText().toString();
+//                break;
+//            case  R.id.button2:
+//                text = ((Button)view).getText().toString();
+//                break;
+//            case  R.id.button3:
+//                text = ((Button)view).getText().toString();
+//                break;
+//            case  R.id.button4:
+//                text = ((Button)view).getText().toString();
+//                break;
+//            case  R.id.button5:
+//                text = ((Button)view).getText().toString();
+//                break;
+//            case  R.id.button6:
+//                text = ((Button)view).getText().toString();
+//                break;
+//                default:
+//                    text = "Nothing";}
+        textView.setText("내가 태어난 곳은 " + text + "입니다.");
 
-        }textView.setText("내가 태어난 곳은 " + text + "입니다.");
+        if(view.getId()==R.id.button7){
+            textView.setText("Birth Place");
+        }
 
     }
+
 }
